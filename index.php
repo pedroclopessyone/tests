0ce49e88cec426a6b17bb4bb5977096caa3add53
Pedro Cravo Lopes <pedro.clopes@syone.com>
2017-12-12 09:29:40 +0000

    redhat.png parameter changed (width to 500)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <h1> DEMO OPENSHIFT - CONTAINER ORCHESTRATION | QLS </h1>
 <br>
 <h2> APP DUMMY FOR QLS DEMO </h2>
-<img src="redhat.png" alt="Kubernetes and Docker" width="450" height="250">
+<img src="redhat.png" alt="Kubernetes and Docker" width="500" height="250">
 <br>
 <img src="openshift.png" alt="Kubernetes and Docker" width="300" height="250">
 <h2><?php
