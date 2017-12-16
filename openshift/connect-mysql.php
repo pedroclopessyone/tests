1d883460dd4cb7a4381466de35ac3c3b5817dc18
pedroclopessyone <pedro.lopes.4@gmail.com>
2017-12-16 19:22:33 +0000

    line 12 connect-mysql.php changed

--- openshift/connect-mysql.php
+++ openshift/connect-mysql.php
@@ -9,7 +9,7 @@ $dbhandle = mysqli_connect($hostname, $username, $password)
 echo "Connected to MySQL<br>";
 
 //select a database to work with
-$selected = mysqli_select_db("examples",$dbhandle)
+$selected = mysqli_select_db("examples", $dbhandle)
   or die("Could not select examples");
 
 //execute the SQL query and return records
