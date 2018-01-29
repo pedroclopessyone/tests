a401908e1ee18c5d7c14e8a7a01d74fee0b11f89
Pedro Cravo Lopes <pedro.clopes@feddy.lopes.local>
2018-01-29 18:46:48 +0000

    connect-mysqli-ocpdemodb-v2.php added

---
+++ openshift/connect-mysqli-ocpdemodb-v2.php
@@ -0,0 +1,28 @@
+ <?php
+$servername = "192.168.42.1";
+$username = "ocpdemodbuser";
+$password = "ocpdemodbpass";
+$dbname = "ocpdemodb";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, model,year,submission_date FROM cars";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo '<i style="color:red;font-size:30px;font-family:calibri ;"> ID: </i> '. $row["id"]. '<i style="color:red;font-size:30px;font-family:calibri ;"> MODEL: </i> '. $row["model"]. " | " . $row["year"]. '<i style="color:red;font-size:30px;font-family:calibri ;"> | Import Date: </i> '. $row["submission_date"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?> 
+
