0850c62372ae9d61645b0df92b951b8b2079e692
Pedro Cravo Lopes <pedro.clopes@feddy.lopes.local>
2018-01-29 18:01:18 +0000

    connect-mysql-ocpdemodb-v2.php added

---
+++ openshift/connect-mysql-ocpdemodb-v2.php
@@ -0,0 +1,18 @@
+<?php
+
+    // Creating a database connection
+
+    $connection = mysqli_connect("192.168.42.1", "ocpdemodbuser", "ocpdemodbpass", "ocpdemodb");
+    if (!$connection) {
+        die("Database connection failed: " . mysqli_connect_error());
+    }
+
+
+    // Selecting a database 
+
+    $db_select = mysqli_select_db($connection, "ocpdemodb");
+    if (!$db_select) {
+        die("Database selection failed: " . mysqli_connect_error());
+    }
+
+?>
