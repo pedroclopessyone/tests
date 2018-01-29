ed1b096120351c397b0b7ab1b1186f7f082a2fdf
Pedro Cravo Lopes <pedro.clopes@feddy.lopes.local>
2018-01-29 18:11:12 +0000

    ocpdemodbuser password updated in connect-mysql-ocpdemodb-v2.php

--- openshift/connect-mysql-ocpdemodb-v2.php
+++ openshift/connect-mysql-ocpdemodb-v2.php
@@ -2,7 +2,7 @@
 
     // Creating a database connection
 
-    $connection = mysqli_connect("192.168.42.1", "ocpdemodbuser", "ocpdemodbuserpass", "ocpdemodb");
+    $connection = mysqli_connect("192.168.42.1", "ocpdemodbuser", "ocpdemodbpass", "ocpdemodb");
     if (!$connection) {
 	    die("Database connection failed: " . mysqli_connect_error());
 	    echo "Connected to MYSQL<br>";
