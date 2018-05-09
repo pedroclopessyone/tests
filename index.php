e4e241f5773292b652af3934d35ae4549538fd65
Pedro Cravo Lopes <pedro.clopes@syone.com>
2018-05-09 23:59:59 +0100

    webhook test2

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 </head>
 
 <body>
-<h1> RHOCP | Syone POC </h1>
+<h1> RHOCP 3.9 | Syone </h1>
 <br>
-<h2> DUMMY APP FOR DEMO PURPOSE </h2>
+<h2> DUMMY PAGE FOR DEMO PURPOSE </h2>
 <img src="redhat.png" alt="Kubernetes and Docker" width="750" height="250">
 <br>
 <img src="openshift.png" alt="Kubernetes and Docker" width="300" height="250">
