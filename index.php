c71be3f11622104023de03b9a3a72e363445b428
Pedro Cravo Lopes <pedro.lopes.4@gmail.com>
2018-10-23 23:59:15 +0100

    index.php line 11 updated

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-<h1><b>RHOCP 3.10 | Syone - Upskill Containers & DevOps by Syone</b></h1>
+<h1><b>RHOCP 3.10 | Upskill by Syone Containers & DevOps by Syone</b></h1>
 <br>
 <h2> WELCOME EVERYONE! </h2>
 <img src="redhat.png" alt="Kubernetes and Docker" width="750" height="250">
